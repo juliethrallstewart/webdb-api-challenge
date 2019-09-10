54caf3cf3c0784700584adfd98192713e6576488
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-09 19:34:09 -0700

    completed MVP and 2 of the stretch questions

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -15,4 +15,79 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('accounts')
+        .where({ id }) 
+        .first() 
+        .then(account => {
+            res.status(200).json(account);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post('/', validate, (req, res) => {
+    const postData = req.body;
+
+    db('accounts')
+        .insert(postData, 'id')
+        .then(([id]) => {
+            db('accounts')
+                .where({ id }) 
+                .first() 
+                .then(post => {
+                    res.status(200).json(post);
+                });
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    db('accounts')
+        .where('id', req.params.id)
+        .update(changes)
+        .then(count => {
+            res.status(200).json({ message: `updated ${count} record` });
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            res.status(200).json({ message: `deleted ${count} records` });
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 module.exports = router;
+
+//middleware 
+
+function validate (req, res, next) {
+
+    const newEntry = req.body
+
+    db('accounts')
+        .select('name', 'budget')
+        .then(result => {
+            console.log(result)
+            !result.includes(newEntry.name) && newEntry.budget ? console.log('account validated', next()):
+            res.status(404).json({message: 'Please enter a name and budget'})
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error retrieving database'})
+        })
+
+}
